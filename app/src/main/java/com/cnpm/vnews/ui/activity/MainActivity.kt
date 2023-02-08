29eab903d103c80620e77ad9373fe41860184073
A38954 <75817929+A38954@users.noreply.github.com>
2023-02-08 15:48:55 +0700

    Build Appbar

--- app/src/main/java/com/cnpm/vnews/ui/activity/MainActivity.kt
+++ app/src/main/java/com/cnpm/vnews/ui/activity/MainActivity.kt
@@ -1,12 +1,49 @@
 package com.cnpm.vnews.ui.activity
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cnpm.vnews.R
+import com.cnpm.vnews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        actionGoToAnotherApp()
     }
+
+    private fun actionGoToAnotherApp() {
+        binding.imageButtonFacebook.setOnClickListener {
+            handlerGoToAnotherApp("https://www.facebook.com/")
+        }
+        binding.imageButtonYouTube.setOnClickListener {
+            handlerGoToAnotherApp("https://www.youtube.com/")
+        }
+        binding.imageButtonTikTok.setOnClickListener {
+            handlerGoToAnotherApp("https://www.tiktok.com/")
+        }
+        binding.imageButtonPhone.setOnClickListener {
+            val phone = "0123456789"
+            val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null))
+            startActivity(intent)
+        }
+        binding.imageButtonLogin.setOnClickListener {
+//            FragmentSetting.openWith(supportFragmentManager)
+        }
+        binding.imageButtonSearch.setOnClickListener {
+//            FragmentSearch.openWith(supportFragmentManager)
+        }
+    }
+
+    private fun handlerGoToAnotherApp(url: String) {
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+    }
+
 }
